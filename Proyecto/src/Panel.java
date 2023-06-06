2e8ef47ea21cf0b7232868782d646573590c2a81
Maticruces <maticruces2021@inf.udec.cl>
2023-06-05 21:31:02 -0400

    agregacion de primeras funciones y creacion de la ventana con el nombre de Simulador de buses

---
+++ Proyecto/src/Panel.java
@@ -0,0 +1,19 @@
+
+import java.awt.*;
+import javax.swing.JPanel;
+
+/*
+*@Panel es una clase que interactua con la ventana
+del expendedor.
+*/
+
+public class Panel extends JPanel{
+    public Panel(int Tamano){
+        super();
+        setBounds(0, 0, 4*Tamano, 3*Tamano);
+        this.setBackground(Color.white);
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+    }
+}
---
+++ Proyecto/src/Ventana.java
@@ -0,0 +1,36 @@
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/*
+*@Ventana es una clase donde se reciben las interacciones entre metodos y funciones
+y esta las pone en funcionamiento.
+*/
+
+public class Ventana extends JFrame{
+    private Panel P;
+    private int escala;
+
+    public Ventana() {
+        super();
+        escala = 160; //con 160 es 640x480 con 240 es 960x720 con 320 es 1280x960
+
+        setSize(4 * escala, 3 * escala); //tamaño fijo de 4:3
+        setTitle("Simulador de buses");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+        this.setResizable(false);
+        crearGUI();
+        setVisible(true);
+    }
+
+    public void crearGUI() {
+    }
+}
